a57b07c55f642d3ae0f3e3fbb6b5c8d97ea2a756
DeepralShakya <shakyadeepral44@gmail.com>
2025-12-01 17:05:53 +0545

    fix: size detail button, responsive

--- src/components/product/ViewSizeGuideButton.tsx
+++ src/components/product/ViewSizeGuideButton.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { Eye } from "lucide-react";
 import ProductImageModal from "../admin/product/ProductImageModal";
 import { Button } from "../ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export default function ViewSizeGuideButton({ image }: { image: string }) {
     const [open, setOpen] = useState(false);
@@ -13,19 +11,12 @@ export default function ViewSizeGuideButton({ image }: { image: string }) {
 
     return (
         <>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Button
-                        variant="outline"
-                        onClick={() => setOpen(true)}
-                        >
-                        <Eye className="w-4 h-4" />
-                    </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                    <p>View Size Guide Image</p>
-                </TooltipContent>
-            </Tooltip>
+            <Button
+                variant="outline"
+                onClick={() => setOpen(true)}
+                >
+                Size Details
+            </Button>
 
             <ProductImageModal
                 open={open}
